df4c0d909cdab75ef9ce3fde809966cfd2475ae6
Viren Patadiya 073 <102893049+VIREN2779@users.noreply.github.com>
2026-01-28 12:39:24 +0000

    feat: add initial data and configure Redux store

---
+++ src/app/initialData.js
@@ -0,0 +1,35 @@
+const intialData = [
+    {
+        unid: '1',
+        firstname: 'pratap',
+        lastname: 'singh',
+        email: 'test1@gmail.com',
+        stateSelect: 'Gujarat',
+        xender: 'Male',
+    },
+    {
+        unid: '2',
+        firstname: 'virat',
+        lastname: 'kohli',
+        email: 'test2@gmail.com',
+        stateSelect: 'Delhi',
+        xender: 'Female',
+    },
+    {
+        unid: '3',
+        firstname: 'pranav',
+        lastname: 'dube',
+        email: 'test3@gmail.com',
+        stateSelect: 'MP',
+        xender: 'Male',
+    },
+    {
+        unid: '4',
+        firstname: 'mahipat',
+        lastname: 'chauhan',
+        email: 'test4@gmail.com',
+        stateSelect: 'Rajasthan',
+        xender: 'Female',
+    },
+];
+export default intialData;
