c1075934ee1b8706adefdadeeb62283491147bd9
Viren Patadiya 073 <102893049+VIREN2779@users.noreply.github.com>
2026-01-28 12:46:26 +0000

    feat(Table.jsx): enhance table component with search functionality and xender filter

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,123 +1,163 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Table() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(10);
-    // console.log(setItemsPerPage);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [xenderFilter, setXenderFilter] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  // console.log(setItemsPerPage);
 
-    const handleAddDataClick = () => {
-        navigate('/form');
-    };
+  const handleAddDataClick = () => {
+    navigate('/form');
+  };
+  // search Data---------------------------------------------------------------------------------------------------------------------------------
+  const inputRef = useRef(null);
+  
+  const searchUserData = () => {
+    setSearchQuery(inputRef.current.value);
+    console.log("search");
+  };
+  // Filter Data--------------------------------------------------------------------------------------------------------------------------------
+  const data = useSelector((state) => state.data);
 
-    // pagination functions ------------------------------------------------------------------------------------------------------------------------------
-    const totalPages = Math.ceil(filteredData.length / itemsPerPage);
-
-    const currentItems = filteredData.slice(
-        (currentPage - 1) * itemsPerPage,
-        currentPage * itemsPerPage
+  const filteredData = data
+    .filter(
+      (item) =>
+        item.firstname.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        item.lastnaem.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        item.email.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    .filter(
+      (item) =>
+        xenderFilter === null ||
+        item.xender.toLowerCase() === xenderFilter.toLowerCase()
     );
 
-    const nextPage = () => {
-        setCurrentPage(currentPage + 1);
-    };
+  const filterUser = (e) => {
+    setXenderFilter(e.target.value);
+  };
+  
 
-    const prevPage = () => {
-        setCurrentPage(currentPage - 1);
-    };
+  // pagination functions ------------------------------------------------------------------------------------------------------------------------------
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
-    return (
-        <>
-            <div className='mt-5 '>
-                <div className='Wdt container'>
-                    <div className='add_btn mt-1 mb-2 d-flex justify-content-around'>
-                        <button
-                            className='btn btn-primary'
-                            onClick={handleAddDataClick}
-                            autoFocus
-                        >
-                            Add Data
-                        </button>
-                        <div className='tablestatus'>
-                            <div className='form-check'>
-                                <input
-                                    className='form-check-input mt-2'
-                                    type='radio'
-                                    name='userXender'
-                                    id='malestatus'
-                                    value='Male'
-                                    onClick={(e) => filterUser(e)}
-                                />
-                                <label className='forn-check-label labelMale'>Male</label>
-                            </div>
-                            <div className='form-check'>
-                                <input
-                                    className='form-check-input mt-2'
-                                    type='radio'
-                                    id='femalestatus'
-                                    name='userXender'
-                                    value='Female'
-                                    onClick={(e) => filterUser(e)}
-                                />
-                                <label className='forn-check-label labelFemale'>Female</label>
-                            </div>
-                        </div>
-                    </div>
-                    <table className='table'>
-                        <thead>
-                            <tr className='table-primary'>
-                                <th scope='col'>Sr.no</th>
-                                <th scope='col'>Id</th>
-                                <th scope='col'>First Name</th>
-                                <th scope='col'>Last Name</th>
-                                <th scope='col'>Email</th>
-                                <th scope='col'>State</th>
-                                <th scope='col'>Xender</th>
-                                <th scope='col' id=''>
-                                    <center>Action</center>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {currentItems.map((item, index) => (
-                                <tr key={index}>
-                                    <td>{(currentPage - 1) * itemsPerPage + index + 1}</td>
-                                    <td>{item.unid}</td>
-                                    <td>{item.firstname}</td>
-                                    <td>{item.lastname}</td>
-                                    <td>{item.email}</td>
-                                    <td>{item.stateSelect}</td>
-                                    <td>{item.xender}</td>
-                                    <td className='headerStatus'>
-                                        status
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-                <div className='pagination-btns'>
-                    <button
-                        disabled={currentPage === 1}
-                        onClick={prevPage}
-                        className='btn btn-primary me-2 previous-btn'
-                    >
-                        Previous Page
-                    </button>
-                    <button
-                        disabled={currentPage === totalPages}
-                        onClick={nextPage}
-                        className='btn btn-primary next-btn'
-                    >
-                        Next Page
-                    </button>
-                </div>
+  const currentItems = filteredData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const prevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  return (
+    <>
+      <div className='mt-5 '>
+        <div className='Wdt container'>
+          <div className='add_btn mt-1 mb-2 d-flex justify-content-around'>
+            <button
+              className='btn btn-primary'
+              onClick={handleAddDataClick}
+              autoFocus
+            >
+              Add Data
+            </button>
+           <form className='form d-flex ' onSubmit={(e) => e.preventDefault()}>
+              <input
+                className='form-control'
+                id='search'
+                type='search'
+                name='search'
+                placeholder='Search data by Name and Location'
+                aria-label='For Search Data'
+                ref={inputRef}
+                onChange={searchUserData}
+              />
+            </form> 
+            <div className='tablestatus'>
+              <div className='form-check'>
+                <input
+                  className='form-check-input mt-2'
+                  type='radio'
+                  name='userXender'
+                  id='malestatus'
+                  value='Male'
+                  onClick={(e) => filterUser(e)}
+                />
+                <label className='forn-check-label labelMale'>Male</label>
+              </div>
+              <div className='form-check'>
+                <input
+                  className='form-check-input mt-2'
+                  type='radio'
+                  id='femalestatus'
+                  name='userXender'
+                  value='Female'
+                  onClick={(e) => filterUser(e)}
+                />
+                <label className='forn-check-label labelFemale'>Female</label>
+              </div>
             </div>
-        </>
-    );
+          </div>
+          <table className='table'>
+            <thead>
+              <tr className='table-primary'>
+                <th scope='col'>Sr.no</th>
+                <th scope='col'>Id</th>
+                <th scope='col'>First Name</th>
+                <th scope='col'>Last Name</th>
+                <th scope='col'>Email</th>
+                <th scope='col'>State</th>
+                <th scope='col'>Xender</th>
+                <th scope='col' id=''>
+                  <center>Action</center>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentItems.map((item, index) => (
+                <tr key={index}>
+                  <td>{(currentPage - 1) * itemsPerPage + index + 1}</td>
+                  <td>{item.unid}</td>
+                  <td>{item.firstname}</td>
+                  <td>{item.lastname}</td>
+                  <td>{item.email}</td>
+                  <td>{item.stateSelect}</td>
+                  <td>{item.xender}</td>
+                  <td className='headerStatus'>
+                                        status
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className='pagination-btns'>
+          <button
+            disabled={currentPage === 1}
+            onClick={prevPage}
+            className='btn btn-primary me-2 previous-btn'
+          >
+            Previous Page
+          </button>
+          <button
+            disabled={currentPage === totalPages}
+            onClick={nextPage}
+            className='btn btn-primary next-btn'
+          >
+            Next Page
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
