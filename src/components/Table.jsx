c8c0d9fafc32ddb041a6a089860ed09f63fa92df
Viren Patadiya 073 <102893049+VIREN2779@users.noreply.github.com>
2026-01-29 06:17:59 +0000

    feat(Table.jsx): add Link import for navigation functionality

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function Table() {
