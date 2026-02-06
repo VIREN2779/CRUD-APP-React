df15513c5b589e9aa9ce80722725facdc6aed483
Viren Patadiya 073 <102893049+VIREN2779@users.noreply.github.com>
2026-02-06 12:59:14 +0000

    #1 feat(EditForm.jsx): Edit user data functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Form from './components/Form';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Table from './components/Table';
 import ViewData from './components/ViewData';
+import EditForm from './components/EditForm';
 
 function App() {
   const [data, setData] = useState([]);
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' element={<Table data={data} />} />
         <Route path='/form' element={<Form onSubmit={handleFormSubmit} />} />
         <Route path='/viewData/:id' element={<ViewData />} />
+        <Route path='/edit/:id' element={<EditForm />} />
       </Routes>
     </Router>
   );
